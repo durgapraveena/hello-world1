20f2f15730abfe6b349f8638361a6841b395a08f
abhic137 <46273637+abhic137@users.noreply.github.com>
2023-07-25 12:09:43 +0530

    Update PalindromeTest.java

--- server/src/test/java/com/example/PalindromeTest.java
+++ server/src/test/java/com/example/PalindromeTest.java
@@ -9,15 +9,39 @@ import org.junit.Test;
 
 public class PalindromeTest{
 
+    /* Sanity - mainline - Regresstion Test 1 - matcH */
 
-@Test
-// Driver code
-public void checkstr()
-{
-    Palindrome palindromeTester = new Palindrome();
-    assertTrue(palindromeTester.isWordPresent(" hello world","hi"));
-	
-	
+	@Test
+    public void testWordInString() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello"));
+    }
+    /* Sanity - mainline - Regresstion Test 2 - miss */
 
-}
+    @Test
+    public void testWordNotInString() {
+        String sentence = "hello world";
+        assertFalse(sentence.contains("cat"));
+    }
+    /* Corner case Test 1 - Empty */
+
+    @Test
+    public void testWordInEmptyString() {
+        String sentence = "";
+        assertFalse(sentence.contains("hello"));
+    }
+    /* Corner case Test 1 - Empty */
+
+    @Test
+    public void testWordInStringCaseInsensitive() {
+        String sentence = "hello world";
+        assertTrue(sentence.toLowerCase().contains("hello"));
+    }
+       /* Corner case Test 1 - Empty */
+ 
+    @Test
+    public void testWordWithSpecialCharacters() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello"));
+    }
 }
