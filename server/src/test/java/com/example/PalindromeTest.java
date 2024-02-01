028209e6e445a6b5debb76a3f41bfbd4bca81e85
Durga Praveena <88362905+durgapraveena@users.noreply.github.com>
2024-02-01 11:29:02 +0530

    Update PalindromeTest.java

--- server/src/test/java/com/example/PalindromeTest.java
+++ server/src/test/java/com/example/PalindromeTest.java
@@ -14,38 +14,38 @@ public class PalindromeTest{
     public void testWordInString() {
         String sentence = "hello world";
         //assertTrue(sentence.contains("hello"));
-	assertTrue(sentence.contains("hai"));
+	//assertTrue(sentence.contains("hai"));
     }
     /* Sanity - mainline - Regresstion Test 2 - miss */
 
     @Test
     public void testWordNotInString() {
         String sentence = "hello world";
-        //assertFalse(sentence.contains("cat"));
-	assertFalse(sentence.contains("hello"));
+        assertFalse(sentence.contains("cat"));
+	//assertFalse(sentence.contains("hello"));
     }
     /* Corner case Test 1 - Empty */
 
     @Test
     public void testWordInEmptyString() {
         String sentence = "";
-        //assertFalse(sentence.contains("hello"));
-	assertFalse(sentence.contains(""));
+        assertFalse(sentence.contains("hello"));
+	//assertFalse(sentence.contains(""));
     }
     /* Corner case Test 1 - Empty */
 
     @Test
     public void testWordInStringCaseInsensitive() {
         String sentence = "hello world";
-        //assertTrue(sentence.toLowerCase().contains("hello"));
-	assertTrue(sentence.toLowerCase().contains("Hello"));
+        assertTrue(sentence.toLowerCase().contains("hello"));
+	//assertTrue(sentence.toLowerCase().contains("Hello"));
     }
        /* Corner case Test 1 - Empty */
  
     @Test
     public void testWordWithSpecialCharacters() {
         String sentence = "hello world";
-        //assertTrue(sentence.contains("hello"));
-	assertTrue(sentence.contains("$hello"));
+        assertTrue(sentence.contains("hello"));
+	//assertTrue(sentence.contains("$hello"));
     }
 }
