3640a8091524247b25a247b6e701c3a540b1fa42
Durga Praveena <88362905+durgapraveena@users.noreply.github.com>
2024-02-01 10:23:54 +0530

    Update PalindromeTest.java

--- server/src/test/java/com/example/PalindromeTest.java
+++ server/src/test/java/com/example/PalindromeTest.java
@@ -1,4 +1,3 @@
-
 package com.example;
 
 import static org.junit.Assert.*;
@@ -9,15 +8,39 @@ import org.junit.Test;
 
 public class PalindromeTest{
 
+    /* Sanity - mainline - Regresstion Test 1 - matcH */
 
-@Test
-// Driver code
-public void checkstr()
-{
-    Palindrome palindromeTester = new Palindrome();
-    assertTrue(palindromeTester.isWordPresent(" hello world","hello"));
-	
-	
+	@Test
+    public void testWordInString() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello"));
+    }
+    /* Sanity - mainline - Regresstion Test 2 - miss */
 
-}
+    @Test
+    public void testWordNotInString() {
+        String sentence = "hello world";
+        assertFalse(sentence.contains("cat"));
+    }
+    /* Corner case Test 1 - Empty */
+
+    @Test
+    public void testWordInEmptyString() {
+        String sentence = "";
+        assertFalse(sentence.contains("hello"));
+    }
+    /* Corner case Test 1 - Empty */
+
+    @Test
+    public void testWordInStringCaseInsensitive() {
+        String sentence = "hello world";
+        assertTrue(sentence.toLowerCase().contains("hello"));
+    }
+       /* Corner case Test 1 - Empty */
+ 
+    @Test
+    public void testWordWithSpecialCharacters() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello"));
+    }
 }
